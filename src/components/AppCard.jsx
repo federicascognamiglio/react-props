b5c5843607990508809c41528618aa7510ef3bbc
federicascognamiglio <scognamigliofede99@gmail.com>
2024-12-13 16:56:37 +0100

    feat: tag colors wip

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -1,22 +1,17 @@
 import AppButton from "./AppButton";
 import style from "./AppCard.module.css";
 
-function AppCard({title, image, content, tags}) {
+function AppCard({ title, image, content, tags }) {
     const imgSrc = image || "https://placehold.co/600x400";
+
     return (
         <div className={style.card}>
-            <div className={style.row}>
-                <div className={style.col60}>
-                    <img src={imgSrc} alt="" />
-                </div>
-                <div className={style.col40}>
-                    <div className={style.description}>
-                        <h4 className={style.title}>{title}</h4>
-                        {tags.map((curTag, index) => <span key={index}>{curTag}</span>)}
-                        <p className={style.caption}>{content}</p>
-                        <AppButton />
-                    </div>
-                </div>
+            <img src={imgSrc} alt="" />
+            <div className={style.description}>
+                <h4 className={style.title}>{title}</h4>
+                {tags.map((curTag, index) => <span key={index} className={`${style.tag} ${style.curTag}`}>{curTag}</span>)}
+                <p className={style.caption}>{content}</p>
+                <AppButton />
             </div>
         </div>
     )
